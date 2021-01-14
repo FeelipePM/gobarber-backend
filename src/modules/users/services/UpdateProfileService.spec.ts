9eb41a1b60bfb00fffab2fc7afa0cef3f3e1fbad
Felipe Machado <feelipe@email.com>
2021-01-14 16:24:29 -0300

    Updating username and email - Ok

---
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,38 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateProfileService from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfile: UpdateProfileService;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    updateProfile = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
+  it('should be able to update profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    const updatedUser = await updateProfile.execute({
+      user_id: user.id,
+      name: 'John Two',
+      email: 'johntwo@gmail.com',
+    });
+
+    expect(updatedUser.name).toBe('John Two');
+    expect(updatedUser.email).toBe('johntwo@gmail.com');
+  });
+});
